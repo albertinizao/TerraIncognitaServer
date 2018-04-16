1ba0a5121a4425b6b0564add828b190baf2d8399
albertinizao <albertinizao@gmail.com>
2018-04-16 21:50:04 +0200

    Start

---
+++ src/main/java/com/opipo/terraincognitaserver/TerraIncognitaServerApplication.java
@@ -0,0 +1,12 @@
+package com.opipo.terraincognitaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraIncognitaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraIncognitaServerApplication.class, args);
+	}
+}
