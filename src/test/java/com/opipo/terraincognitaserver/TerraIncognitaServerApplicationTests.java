6a67af7f05b8d6b2300d0720087448ac8abe8262
SE05884 <SE05884@PB033SU9.bankinter.bk>
2018-04-17 12:14:33 +0200

    Junit 5 and swagger

--- src/test/java/com/opipo/terraincognitaserver/TerraIncognitaServerApplicationTests.java
+++ src/test/java/com/opipo/terraincognitaserver/TerraIncognitaServerApplicationTests.java
@@ -1,16 +1,78 @@
 package com.opipo.terraincognitaserver;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+import static org.junit.jupiter.api.Assumptions.assumingThat;
+//import org.junit.jupiter.api.;
+
 public class TerraIncognitaServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @BeforeAll
+    static void initAll() {
+    }
+
+    @BeforeEach
+    void init() {
+    }
+
+    @Test
+    void succeedingTest() {
+    	assertEquals(2,1);
+    }
+
+    @Test
+    @Disabled("for demonstration purposes")
+    void skippedTest() {
+        // not executed
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+    }
+
+    @Test
+    void assertWithHamcrestMatcher() {
+        assertThat(2 + 1, is(equalTo(3)));
+    }
+    @Test
+    void testOnlyOnCiServer() {
+        assumeTrue("CI".equals(System.getenv("ENV")));
+        // remainder of test
+    }
+
+    @Test
+    void testOnlyOnDeveloperWorkstation() {
+        assumeTrue("DEV".equals(System.getenv("ENV")),
+            () -> "Aborting test: not on developer workstation");
+        // remainder of test
+    }
+
+    @Test
+    void testInAllEnvironments() {
+        assumingThat("CI".equals(System.getenv("ENV")),
+            () -> {
+                // perform these assertions only on the CI server
+                assertEquals(2, 2);
+            });
+
+        // perform these assertions in all environments
+        assertEquals("a string", "a string");
+    }
+
+
 
 }
