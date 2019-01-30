543a0cd804987fcb33ca74b8b8a88370331a0b47
acebrian <alberto.cebrian@atmira.com>
2019-01-30 16:04:49 +0100

    intento de cobertura de integración

--- src/test/java/com/opipo/terraincognitaserver/it/CucumberTest.java
+++ src/test/java/com/opipo/terraincognitaserver/it/CucumberTest.java
@@ -7,6 +7,6 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/resources/features"}, format = {"pretty", "html:target/reports/cucumber/html",
-        "json:target/cucumber.json", "usage:target/usage.jsonx", "junit:target/junit.xml"})
+        "json:target/cucumber.json", "usage:target/usage.jsonx", "junit:target/junit.xml"} )
 public class CucumberTest {
 }
